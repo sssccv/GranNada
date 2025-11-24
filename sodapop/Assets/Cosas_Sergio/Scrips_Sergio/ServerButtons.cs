344d956787c5a8a675c616176e824499285b5ead
Sergio Salazar <sssr308a@gmail.com>
2025-11-24 16:20:50 -0600

    ip input

--- sodapop/Assets/Cosas_Sergio/Scrips_Sergio/ServerButtons.cs
+++ sodapop/Assets/Cosas_Sergio/Scrips_Sergio/ServerButtons.cs
@@ -1,39 +1,40 @@
-using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP; // Necesario
+using UnityEngine;
+using UnityEngine.UI; // Para usar InputFields
+
 
 public class ServerButtons : MonoBehaviour
 {
-    //[SerializeField] private GameObject panel;
-    //[SerializeField] private GameObject button;
+    public TMPro.TMP_InputField ipAddressInput;
 
-    /*private void Start()
+    private void Start()
     {
-        panel.SetActive(false);
-        button.SetActive(true);
+        // Por defecto ponemos localhost para pruebas rápidas
+        ipAddressInput.text = "127.0.0.1";
     }
-
-    public void ActivatePanel()
-    {
-        panel.SetActive(true);
-        button.SetActive(false);
-    }
-
-    public void DeactivatePanel()
-    {
-        panel.SetActive(false);
-        button.SetActive(true);
-    }*/
 
     public void HostServer()
     {
         NetworkManager.Singleton.StartHost();
+
     }
     public void JoinServer()
     {
+        // 1. Obtenemos el componente de transporte
+        var transporte = NetworkManager.Singleton.GetComponent<UnityTransport>();
+
+        // 2. Le decimos a qué IP conectarse (la que escribiste en el InputField)
+        // El puerto por defecto de Netcode es 7777, asegúrate de no haberlo cambiado
+        transporte.SetConnectionData(
+            ipAddressInput.text,  // La IP del Host (ej: 192.168.1.50)
+            7777                  // El puerto (ushort)
+        );
+
+        // 3. Iniciamos el cliente
         NetworkManager.Singleton.StartClient();
     }
+
     public void CloseServer()
     {
         NetworkManager.Singleton.Shutdown();
